fc11bf8af135927a4905a6efcdb6f60fd8310fb2
valentina cantarelli <valentinacanterina@gmail.com>
2021-07-28 14:57:09 +0200

    password

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var nome = prompt ('Ciao! Dimmi il tuo nome');
+alert
+var cognome = prompt ('Ora il tuo cognome');
+
+var colore  = prompt ('Ora invece il tuo colore preferito');
+
+var animale = prompt ('Il tuo animale preferito')
+
+var password = nome + cognome + colore + animale;
+
+document.getElementById ('psw').innerHTML += password + ('40') ;
