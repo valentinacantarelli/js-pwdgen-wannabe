5c98254f8c1c020b979da5b038fcf62e9a40ad2f
valentina cantarelli <valentinacanterina@gmail.com>
2021-07-28 17:52:02 +0200

    modificata grafica

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-var nome = prompt ('Ciao! Dimmi il tuo nome');
-alert
+alert('Siamo qui per aiutarti a creare la password dei tuoi sogni! Dovrai rispondere solo a qualche domanda...iniziamo!')
+
+var nome = prompt ('Dicci il tuo nome');
+
 var cognome = prompt ('Ora il tuo cognome');
 
 var colore  = prompt ('Ora invece il tuo colore preferito');
 
-var animale = prompt ('Il tuo animale preferito')
+var animale = prompt ('E per ultimo il tuo animale preferito')
 
 var password = nome + cognome + colore + animale;
 
